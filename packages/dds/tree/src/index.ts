9e85a86145de7880ff237dc3ef994718cee6d7ef
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-10-05 19:13:39 -0700

    Move dataStoreKind

--- packages/dds/tree/src/index.ts
+++ packages/dds/tree/src/index.ts
@@ -405,9 +405,3 @@ export { ExtensibleUnionNode } from "./extensibleUnionNode.js";
 
 export { treeDataStoreKind } from "./treeDataStore.js";
 export type { TreeDataStoreOptions } from "./treeDataStore.js";
-export { dataStoreKind } from "./dataStoreKind.js";
-export type {
-	DataStoreOptions,
-	SharedObjectCreator,
-	SharedObjectRegistry,
-} from "./dataStoreKind.js";
